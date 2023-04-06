93d7e01a3def8d91661cd83d9473b4e04b95771a
Matthew Wolfe <thewolfeslayer@gmail.com>
2023-04-06 19:22:38 -0400

    made character

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
 const gameArea = document.querySelector('canvas');
 
+const ctx = gameArea.getContext('2d')
+
 gameArea.width = innerWidth
 gameArea.height = innerHeight
+
+class Player {
+    constructor(x, y, radius, color) {
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.color = color
+    }
+
+    draw() {
+        ctx.beginPath()
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+        ctx.fillStyle = this.color
+        ctx.fill()
+    }
+}
+
+const x = gameArea.width / 2
+const y = gameArea.height / 2
+const player = new Player(x, y, 50, 'red')
+player.draw()
