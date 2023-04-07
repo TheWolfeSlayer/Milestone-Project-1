dfc1917b1d5d1cca3436a7acc577f5916567c42b
Matthew Wolfe <thewolfeslayer@gmail.com>
2023-04-06 20:17:42 -0400

    made projectile class

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const ctx = gameArea.getContext('2d')
 gameArea.width = innerWidth
 gameArea.height = innerHeight
 
+
+
 class Player {
     constructor(x, y, radius, color) {
         this.x = x
@@ -21,7 +23,50 @@ class Player {
     }
 }
 
+class Projectile {
+    constructor(x, y, radius, color, velocity) {
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.color = color
+        this.velocity = velocity
+        
+    }
+    draw() {
+        ctx.beginPath()
+        ctx.arc(player.x, player.y, this.radius, 0, Math.PI * 2, false)
+        ctx.fillStyle = this.color
+        ctx.fill()
+    }
+
+    update() {
+        this.x = this.x + this.velocity.x
+        this.y = this.y + this.velocity.y
+    }
+}
+
 const x = gameArea.width / 2
 const y = gameArea.height / 2
 const player = new Player(x, y, 50, 'red')
 player.draw()
+
+const projectile = new Projectile(
+    x, 
+    y, 
+    10, 
+    'blue',
+    {
+        x: 1,
+        y: 1
+    }
+)
+
+function animate(){
+    requestAnimationFrame(animate)
+    projectile.draw()
+    projectile.update()
+}
+
+window.addEventListener('click', (event) => {
+    animate()
+})
