b435abed0f5e057f0df7bbfb35b35dd62ec14b82
Matthew Wolfe <thewolfeslayer@gmail.com>
2023-04-13 16:55:35 -0400

    added comments to describe what is being done

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const gameArea = document.querySelector('canvas');
 
 const ctx = gameArea.getContext('2d')
 
+//builds image variable to hold sprite
 var image = document.createElement('img')
 image.src = 'assets/Adventurer.png'
+//makes canvas the full screen
 gameArea.width = innerWidth
 gameArea.height = innerHeight
 
 class Player {
+    //assigns values to player
     constructor(x, y, radius, color) {
         this.x = x
         this.y = y
@@ -16,6 +19,7 @@ class Player {
     }
 
     draw() {
+        //draws sprite, flips depending on which side of the screen is clicked
         if (clickX < x) {
             image.src = 'assets/AdventurerFlipped.png'
             ctx.drawImage(image, x-30, y-25, 75, 55.5)
@@ -39,15 +43,15 @@ class Projectile {
         this.radius = radius
         this.color = color
         this.velocity = velocity
-        
     }
+    //draws the projectile to the canvas
     draw() {
         ctx.beginPath()
         ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
         ctx.fillStyle = this.color
         ctx.fill()
     }
-
+    //allows the projectile to move
     update() {
         this.draw()
         this.x = this.x + this.velocity.x
@@ -56,21 +60,22 @@ class Projectile {
 }
 
 class enemy {
+    //assigns values to enemy
     constructor(x, y, radius, color, velocity) {
         this.x = x
         this.y = y
         this.radius = radius
         this.color = color
         this.velocity = velocity
-        
     }
+    //draws enemy to screen
     draw() {
         ctx.beginPath()
         ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
         ctx.fillStyle = this.color
         ctx.fill()
     }
-
+    //allows enemy to move on the canvas
     update() {
         this.draw()
         this.x = this.x + this.velocity.x
@@ -78,13 +83,18 @@ class enemy {
     }
 }
 
+//takes the midpoint of the screen and assigns it to variables
 const x = gameArea.width / 2
 const y = gameArea.height / 2
+//makes player on screen
 const player = new Player(x, y, 45, 'red')
+//makes array to hold multiple projectiles and enemies
 const projectiles = []
 const enemies = []
+//makes variable that is later used to determine if the player sprite needs to be flipped
 let clickX = 0
 
+//function to spawn enemies on the screen
 function spawnEnemies() {
     setInterval(() => {
         const x = 100
@@ -99,6 +109,7 @@ function spawnEnemies() {
     }, 1000)
 }
 
+//animates everything on the canvas
 function animate(){
     requestAnimationFrame(animate)
     ctx.clearRect(0, 0, gameArea.width, gameArea.height)
